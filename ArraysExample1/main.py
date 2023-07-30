b3bbf2e1676ffed142aa46f7f4c238af5125877b
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-30 13:56:18 +0200

    Update main.py

--- ArraysExample1/main.py
+++ ArraysExample1/main.py
@@ -3,4 +3,4 @@ second_array = ["One", "Two", "Three", "Four", "Five",
                 "Six", "Seven", "Eight", "Nine", "Ten"]
 
 print("First array: {} \n"
-      "Second array: {}".format(first_array,second_array))
+      "Second array: {}".format(first_array, second_array))

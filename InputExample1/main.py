d719bbce9dace80dae2381089b98a1416cd34379
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-31 15:26:33 +0200

    Update main.py
    Added if name is empty check

--- InputExample1/main.py
+++ InputExample1/main.py
@@ -1,3 +1,7 @@
 import _datetime
 name = input("What's your name?: ")
-print("Hello {}! The date and time is {} ".format(name, _datetime.datetime.today()))
+
+if name == "":
+    print("You forgot to enter your name!")
+else:
+    print("Hello {}! The date and time is {} ".format(name, _datetime.datetime.today()))

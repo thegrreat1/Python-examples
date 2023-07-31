f7459cb94655f629e4f5153360600b11944d2b57
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-31 15:44:58 +0200

    Why not?

---
+++ DwightKShruteRandomQuote/main.py
@@ -0,0 +1,24 @@
+import random
+quote = ["Whenever I'm about to do something, I think, 'Would an idiot do that?' And if they would, I do not do that thing",
+         "Always the Padawan, never the Jedi.",
+         "There’s too many people on this earth. We need a new plague.",
+         "Today, smoking is going to save lives.",
+         "You couldn’t handle my undivided attention.",
+         "People underestimate the power of nostalgia. Nostalgia is truly one of the greatest human weaknesses, second only to the neck.",
+         "All you need is love? False. The four basic human necessities are air, water, food and shelter.",
+         "I am fast. To give you a reference point, I'm somewhere between a snake and a mongoose… and a panther.",
+         "Those who can’t farm, farm celery.",
+         "I signed up for Second Life about a year ago. Back then, my life was so great that I literally wanted a second one. Absolutely everything was the same. Except I could fly.",
+         "Yes, I have a wig for every single person in the office. You never know when you're gonna need to bear a passing resemblance to someone.",
+         "The eyes are the groin of the head.",
+         "As a farmer, I know that when an animal is sick sometimes the right thing to do is to put it out of its misery. With the electricity we're using to keep Meredith alive, we could power a small fan for two days. You tell me what’s unethical.",
+         "Five minutes ahead of schedule. Right on schedule.",
+         "You know, I really would’ve appreciated a heads up that you were into dating mothers. I would’ve introduced you to mine.",
+         "Welcome to the Hotel Hell. Check-in time is now. Check-out time is never.",
+         "Twelve hundred dollars is what I spent on my whole bomb shelter. For that kind of money, this stroller, should be indestructible.",
+         "Jim is my enemy. But it turns out that Jim is also his own worst enemy. And the enemy of my enemy is my friend. So Jim is actually my friend. But, because he is his own worst enemy, the enemy of my friend is my enemy, so actually, Jim is my enemy..",
+         "There are forty rules all Schrute boys must learn before the age of five. Learn your rules. You better learn your rules. If you don't you'll be eaten in your sleep!",
+         "I wish I could menstruate. If I could menstruate, I wouldn't have to deal with idiotic calendars anymore. I'd just be able to count down from my previous cycle. Plus, I'd be more in tune with the moon and the tides."
+         ]
+
+print("Dwigkt K. Schrute Quote of the day:\n {}".format(random.choice(quote)))

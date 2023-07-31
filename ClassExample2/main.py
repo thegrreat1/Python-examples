3bb12d191697c7d224d5d781c4a54ad3e6693255
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-31 14:06:38 +0200

    Added ClassExample2

---
+++ ClassExample2/main.py
@@ -0,0 +1,7 @@
+
+class WelcomeMessage:
+    def __init__(self):
+        self.message = "Welcome to the Hotel Hell. Check-in time is now. Check-out time is never."
+
+
+print(WelcomeMessage().message)
